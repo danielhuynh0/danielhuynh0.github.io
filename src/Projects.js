614535e1cbdf5f332b5d050ddc1bc9a068fdcc64
Daniel Huynh <danielhuynh523@gmail.com>
2023-07-20 22:23:13 -0400

    made the page more responsive to smaller screens

--- src/Projects.js
+++ src/Projects.js
@@ -72,7 +72,7 @@ const Projects = React.forwardRef((props, ref) => {
       
       const Portfolio = () => (
           <div>
-            <div className="max-w-6xl mx-auto px-4 sm:mt-32">
+            <div className="max-w-6xl mx-auto px-4 lg:mt-8 md:mt-12 sm:mt-32">
               <h1 className="text-5xl font-bold text-white mb-12 text-center">Projects</h1>
               <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-14">
                 {projects.map((project) => (
